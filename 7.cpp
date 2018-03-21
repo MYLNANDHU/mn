060787dc7c55782e4943d542d8c4adfb7996e3ca
MYLNANDHU <36394363+MYLNANDHU@users.noreply.github.com>
2018-03-21 11:22:32 +0530

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char str[6]="HELLO";
+	int num,i;
+	cout<<"Enter a number\n";
+	cin>>num;
+	for(i=0;i<num;i++)
+	cout<<str<<"\n";
+	return 0;
+}
