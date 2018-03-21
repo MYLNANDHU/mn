babd783692ae6f6d04d672b1e8b744c2df7c8e7e
MYLNANDHU <36394363+MYLNANDHU@users.noreply.github.com>
2018-03-21 11:29:28 +0530

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+using namespace std;
+int main(){
+	int n,count=0;
+	cout<<"Enter the digits of number\n";
+	cin>>n;
+	cout<<"Entered number:"<<num<<"\n";
+	while(n!=0)
+	{
+		n=n/10;
+		count+=1;
+	}
+	cout<<count;
+	return 0;
+}
