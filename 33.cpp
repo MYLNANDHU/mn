4c34502a36e9dade8765ae9345388b4d7a4aaaad
MYTHILI <36394363+MYLNANDHU@users.noreply.github.com>
2018-05-21 10:32:58 +0530

    Create 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int count=0,i;
+char str[1000];
+cout<<"enter the line";
+cin.getline(str,1000)
+for(i=0;str[i]!='/0';i++)
+{
+cout<<count;
+return 0;
+}
+}
