70d1ec4d056688b704e7e962f6ffb2ff328eee46
MYTHILI <36394363+MYLNANDHU@users.noreply.github.com>
2018-05-21 12:35:40 +0530

    Create 38.cpp

---
+++ 38.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout << "Enter two numbers to be swapped : ";
+    cin >> a >> b;
+    c=^a;
+    a=^b;
+    b^=c;
+    cout<<a<<b;
+    return 0;
+}
